f26362a794b0da015a727dbc734f7fe894f40741
Naz <bluspringindohq@gmail.com>
2023-08-13 05:16:33 +0800

    make accessor mutable

--- src/main/java/com/github/alexthe666/citadel/mixin/TimerAccessor.java
+++ src/main/java/com/github/alexthe666/citadel/mixin/TimerAccessor.java
@@ -2,6 +2,7 @@ package com.github.alexthe666.citadel.mixin;
 
 import net.minecraft.client.Timer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(Timer.class)
@@ -10,5 +11,6 @@ public interface TimerAccessor {
     float getMsPerTick();
 
     @Accessor
+    @Mutable
     void setMsPerTick(float value);
 }
